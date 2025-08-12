6954f7b68e07ff3ba121e03a094873ef0c7ab405
Akeeb Ismail <damiz.akeeb@gmail.com>
2025-08-12 16:12:07 +0100

    published proto

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.proto = void 0;
 const path = require("path");
 const proto = (name) => {
-    const protoPath = path.resolve(__dirname, '..', 'proto', name, `${name}.proto`);
+    const protoPath = path.resolve(__dirname, '..', 'proto', name);
     return protoPath;
 };
 exports.proto = proto;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as path from 'path';
 export const proto = (name: string): string => {
   // Use __dirname to get the directory of the current file
   // Then navigate to the proto folder relative to this package
-  const protoPath = path.resolve(__dirname, '..', 'proto', name, `${name}.proto`);
+  const protoPath = path.resolve(__dirname, '..', 'proto', name);
   return protoPath;
 }
