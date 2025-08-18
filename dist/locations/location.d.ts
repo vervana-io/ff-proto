0a09d8698843c052df92c17bd21f5649e752a86e
Akeeb Ismail <damiz.akeeb@gmail.com>
2025-08-18 09:33:05 +0100

    all users service proto

--- dist/locations/location.d.ts
+++ dist/locations/location.d.ts
@@ -13,7 +13,6 @@ export interface Address {
     city?: string | undefined;
     state?: string | undefined;
     country?: string | undefined;
-    isPrimary: boolean;
     metadata: {
         [key: string]: string;
     };
